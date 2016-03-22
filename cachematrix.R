e501f3a6771407a2db03d804b20ed0fed16c4cad
Javier Montesinos <fj.montesinos@preving.com>
2016-03-22 22:52:51 +0100

    UPDATED. Se añade el código de la función cacheSolve

--- cachematrix.R
+++ cachematrix.R
@@ -19,8 +19,40 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
+## Esta función obtiene la inversa de una matriz generada con makeCacheMatrix
+# Evalua si el argumento pasado como primer parámetro es una matrix cuadrada 
+# no singular para poder calcular su inversa.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  
+  # El argumento x no es una matriz
+  if(!is.matrix(x$get())) {
+    stop("El argumento no es una matriz")
+  }
+  
+  # El argumento x no es una matriz cuadrada
+  if(ncol(x$get()) != nrow(x$get())){
+    stop ("El argumento no es una matriz cuadrada")
+  }
+  
+  # Si el determinante de la matriz es 0 -> es singular -> no podremos
+  # obtener la matriz inversa -> reportamos el error controlado
+  if(det(x$get()) == 0){
+    stop("El argumento x es una matriz singular")
+  }
+  
+  s <- x$getsolve()
+  
+  # Si el objeto pasado como parámetro ya ha calculado la matriz inverse ->
+  # se retorna el valor en cache, i.e. no se calcula la inversa de la matriz.
+  if(!is.null(s)) {
+    message("Obteniendo valor de cache del objeto")
+    return(s)
+  }
+  
+  data <- x$get()
+  s <- solve(data, ...)
+  x$setsolve(s)
+  s
 }
