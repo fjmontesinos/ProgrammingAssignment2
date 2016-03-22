facb85f1e6d99a8816421a7eef55ade97f29ffb6
Javier Montesinos <fj.montesinos@preving.com>
2016-03-22 22:49:25 +0100

    UPDATED. Se añade el código de la función makeCacheMatrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,10 +1,21 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Fco. Javier Montesinos
+## 22 de marzo de 2016
+## Programming Assignment 2: Lexical Scoping
 
-## Write a short comment describing this function
+## Esta funcion almacena una matriz y su inversa
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  s <- NULL
+  set <- function(y) {
+    x <<- y
+    s <<- NULL
+  }
+  get <- function() x
+  setsolve <- function(solve) s <<- solve
+  getsolve <- function() s
+  list(set = set, get = get,
+       setsolve = setsolve,
+       getsolve = getsolve)
 }
 
 
